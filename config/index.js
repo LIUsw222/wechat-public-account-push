8f0fe97e34292e46c1aad1b0170e4716163c36d3
LIUsw222 <112595755+LIUsw222@users.noreply.github.com>
2022-09-01 09:10:20 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,13 +4,13 @@ export const config = {
      */
 
     // 公众号APP_ID
-    APP_ID: "",
+    APP_ID: "wx3ed45903f6f6e098",
 
     // 公众号APP_SECRET
-    APP_SECRET: "",
+    APP_SECRET: "cf0d3ea1cd8921a1c339011275d4508e",
 
     // 模板消息id
-    TEMPLATE_ID: "xxxxxxxxxxxxx-y你懂的,填错狗头打烂VRMpu",
+    TEMPLATE_ID: "徐雨轩女士，早上好！",
 
     // 回调消息模板id, 用来看自己有没有发送成功的那个模板
     CALLBACK_TEMPLATE_ID: "xxxxxxxxxxxxx-y你懂的,填错狗头打烂VRMpu",
@@ -32,7 +32,7 @@ export const config = {
     ],
     // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
     CALLBACK_USERS: [
-      {name: '自己',id: "5hxOO6VskYv你懂的,填错狗头打烂-ywIZVRMpuXhk"}, 
+      {name: '刘书玮',id: "oX1Ng57GxOw4Lg_LiFTTrcI61Iok"}, 
     ],
      
     /**
@@ -42,9 +42,9 @@ export const config = {
     /** 天气相关 */
 
     // 所在省份
-    PROVINCE: "广东",
+    PROVINCE: "江苏",
     // 所在城市
-    CITY: "惠州",
+    CITY: "丹阳",
 
     /** 重要节日相关 */
 
@@ -53,10 +53,10 @@ export const config = {
     // 生日时，name填写想要展示的名字，你可以填“美丽可爱亲亲老婆”
     // 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
     FESTIVALS: [
-      {"type": "生日", "name": "老婆", "year": "1996", "date": "01-01"},
-      {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "01-04"},
-      {"type": "生日", "name": "李四", "year": "1996", "date": "08-29"},
-      {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "08-29"},
+      {"type": "生日", "name": "老婆", "year": "2002", "date": "01-19"},
+      {"type": "生日", "name": "刘书玮", "year": "2001", "date": "01-25"},
+      {"type": "节日", "name": "在一起的纪念日", "year": "2017", "date": "08-28"},
+      //{"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "08-29"},
     ],
     // 限制重要节日的展示条目, 需要填写数字; 
     // 如果为3, 则仅展示“将要到达” 的3个重要节日提醒，剩下的将被忽略
@@ -66,9 +66,9 @@ export const config = {
     /** 日期相关 */
 
     // 在一起的日子，格式同上
-    LOVE_DATE: "2015-05-01",
+    LOVE_DATE: "2017-08-28",
     // 结婚纪念日
-    MARRY_DATE: "2020-01-04",
+ 
 
 
     /** 每日一言 */
@@ -81,12 +81,12 @@ export const config = {
     }
 
 // {{date.DATA}}  
-// 城市：{{city.DATA}}  
-// 天气：{{weather.DATA}}  
-// 最低气温: {{min_temperature.DATA}}  
-// 最高气温: {{max_temperature.DATA}}  
-// 今天是我们恋爱的第{{love_day.DATA}}天
-// 今天是我们结婚的第{{marry_day.DATA}}天
+城市：{{city.DATA}}  
+天气：{{weather.DATA}}  
+最低气温: {{min_temperature.DATA}}  
+最高气温: {{max_temperature.DATA}}  
+今天是我们恋爱的第{{love_day.DATA}}天
+今天是我们结婚的第{{marry_day.DATA}}天
 // {{birthday_message.DATA}}
 
 // {{note_en.DATA}}  
